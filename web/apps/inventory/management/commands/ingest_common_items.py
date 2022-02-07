2dd9599da5d33901188f55f9a5e42f25cbaa18f1
Adam Torres <adamprogrammer@gmail.com>
2022-02-06 18:19:45 -0700

    More category/common name ingest work.

--- web/apps/inventory/management/commands/ingest_common_items.py
+++ web/apps/inventory/management/commands/ingest_common_items.py
@@ -32,7 +32,7 @@ class Command(BaseCommand):
         print(f"Total records: {data['records']}")
         for category, category_data in data["categories"].items():
             print(f"\tCategory: {category}")
-            print(f"\tRecords: {category_data['records']}")
+            print(f"\tRecords: {len(category_data)}")
         print(f"Common names: {len(data['common_names'])}")
 
     def handle(self, *args, **options):
@@ -47,7 +47,9 @@ class Command(BaseCommand):
         }
         self.process_datafile(datafile, self.process_row, args=args)
         self.dump_stats(data)
-        self.upsert_common_items(data)
+        self.upsert_categories(data["categories"])
+        self.upsert_common_items(data["common_names"])
+        self.update_categories(data["categories"])
 
     def process_datafile(self, datafile, row_func, args=None, skip_first_row=True):
         with open(datafile, 'r') as csvfile:
@@ -67,26 +69,70 @@ class Command(BaseCommand):
                 "orange juice carton": {"oj carton", },
             }
         }
+        self.process_categories(row, data["categories"])
+        self.process_common_names(row, data["common_names"])
+
+    def process_categories(self, row, categories):
+        # Goal: keys = category names.  values = set of common names.
+        cat_lower = row.category.lower()
+        if cat_lower not in categories:
+            categories[cat_lower] = set()
+        categories[cat_lower].add(row.common_name.lower())
+
+    def process_common_names(self, row, common_names):
         cn_lower = row.common_name.lower()
-        if cn_lower not in data["common_names"]:
-            data["common_names"][cn_lower] = set()
+        if cn_lower not in common_names:
+            common_names[cn_lower] = set()
         # Update the set for the common name with a lower/strip set of other names.  Excludes blanks.
-        data["common_names"][cn_lower].update([
+        common_names[cn_lower].update([
             n.lower().strip() for n in [row.first_other_name, row.second_other_name, row.third_other_name]
             if n.strip()])
 
-    def upsert_common_items(self, data):
-        existing_common_names = set(
-            inv_models.CommonItem.objects.filter(name__in=data["common_names"]).values_list('name', flat=True))
-        print(f"existing common names from file: {len(existing_common_names)}")
-        new_common_names = set(data["common_names"]).difference(existing_common_names)
-        print(f"new common names from file: {len(new_common_names)}")
-        if new_common_names:
-            new_common_items = [inv_models.CommonItem(name=n) for n in new_common_names]
-            inv_models.CommonItem.objects.bulk_create(new_common_items)
+    def generic_upsert(self, model, new_data, data_name):
+        existing_values = set(model.objects.filter(name__in=new_data).values_list('name', flat=True))
+        print(f"existing {data_name} values from file: {len(existing_values)}")
+        new_values = set(new_data).difference(existing_values)
+        print(f"new {data_name} values from file: {len(new_values)}")
+        if new_values:
+            new_items = [inv_models.CommonItem(name=n) for n in new_values]
+            model.objects.bulk_create(new_items)
+
+    def update_categories(self, categories):
+        for category, category_data in categories.items():
+            print(f"Category: {category}")
+            print(f"\tFile contains {len(category_data)} common names")
+            category_obj = inv_models.Category.objects.filter(name=category).first()
+            existing_names = category_obj.common_items.filter(name__in=category_data).values_list('name', flat=True)
+            print(f"\tExisting {len(existing_names)} common items on category")
+            new_names = set(category_data).difference(existing_names)
+            print(f"\tAdding {len(new_names)} common items to category")
+            new_common_items = inv_models.CommonItem.objects.filter(name__in=new_names)
+            category_obj.common_items.add(*list(new_common_items))
+
+    def upsert_categories(self, categories):
+        # existing_common_names = set(
+        #     inv_models.Category.objects.filter(name__in=categories).values_list('name', flat=True))
+        # print(f"existing common names from file: {len(existing_common_names)}")
+        # new_common_names = set(categories).difference(existing_common_names)
+        # print(f"new common names from file: {len(new_common_names)}")
+        # if new_common_names:
+        #     new_common_items = [inv_models.Category(name=n) for n in new_common_names]
+        #     inv_models.Category.objects.bulk_create(new_common_items)
+        self.generic_upsert(inv_models.Category, categories, "category")
+
+    def upsert_common_items(self, common_names):
+        self.generic_upsert(inv_models.CommonItem, common_names, "common name")
+        # existing_common_names = set(
+        #     inv_models.CommonItem.objects.filter(name__in=common_names).values_list('name', flat=True))
+        # print(f"existing common names from file: {len(existing_common_names)}")
+        # new_common_names = set(common_names).difference(existing_common_names)
+        # print(f"new common names from file: {len(new_common_names)}")
+        # if new_common_names:
+        #     new_common_items = [inv_models.CommonItem(name=n) for n in new_common_names]
+        #     inv_models.CommonItem.objects.bulk_create(new_common_items)
 
         new_other_item_names = []
-        for common_name, other_names in data["common_names"].items():
+        for common_name, other_names in common_names.items():
             if not other_names:
                 continue
             ci = inv_models.CommonItem.objects.get(name=common_name)
