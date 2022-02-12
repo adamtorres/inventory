aad62513b2f1537214282d3235907216eaa47db3
Adam Torres <adamprogrammer@gmail.com>
2022-02-11 19:38:12 -0700

    Added IncomingGroupDetailView to show details of an individual IncomingGroup. Moved the dollar line css to base so more pages could use it. Only able to add the link to converted rows until I work out how to do the form without formsets.

--- web/apps/incoming/urls.py
+++ web/apps/incoming/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('incominggroups/', views.IncomingGroupListView.as_view(), name="incoming_groups"),
+    path('incominggroup/<uuid:pk>/', views.IncomingGroupDetailView.as_view(), name='incoming_group'),
 ]
---
+++ web/apps/incoming/views/incoming_group_list_generic.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from incoming import models as inc_models
+
+
+class IncomingGroupView(ListView):
+    """
+    Using the ListView to show the IIGs instead of manually making everthing.
+    """
+    model = inc_models.IncomingItemGroup
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.order_by('-action_date')
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        return context
