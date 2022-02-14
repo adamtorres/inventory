c4ca7fad6226bed0fcdc7813e4c4cfcc51d04712
Adam Torres <adamprogrammer@gmail.com>
2022-02-13 21:41:22 -0700

    Added create and edit views/forms based on an inline formset example.  Lots of example code still here. Added dotenv to asgi as I just noticed it was missing.  Won't use it but wanted to be complete. Changed cancel buttons to links as it was failing form validation. Added create links to the group list page and the base.

--- web/apps/incoming/urls.py
+++ web/apps/incoming/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('incominggroup/<uuid:pk>/edit', views.IncomingGroupUpdateView.as_view(), name='incoming_group_edit'),
     path('incominggroup/<uuid:pk>/', views.IncomingGroupDetailView.as_view(), name='incoming_group'),
     path('incominggroup/', views.IncomingGroupCreateView.as_view(), name='incoming_group_create'),
+    path('incominggroup/ex_create/', views.ExampleIncomingItemGroupCreateView.as_view(), name='ex_create'),
+    path('incominggroup/<uuid:pk>/ex_edit', views.ExampleIncomingItemGroupEditView.as_view(), name='ex_edit'),
 ]
--- web/apps/incoming/views/__init__.py
+++ web/apps/incoming/views/__init__.py
@@ -3,3 +3,4 @@ from .incoming_group_detail import IncomingGroupDetailView
 from .incoming_group_list_generic import IncomingGroupView
 from .incoming_group_edit import IncomingGroupUpdateView
 from .incoming_group_create import IncomingGroupCreateView
+from .from_example import ExampleIncomingItemGroupEditView, ExampleIncomingItemGroupCreateView
