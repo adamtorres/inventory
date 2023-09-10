9304f620e14effae98835506719dbd3d9fe24104
Adam Torres <adamprogrammer@gmail.com>
2023-09-10 00:18:05 -0600

    Fleshed out the test for get_combined_filter.  Validates the Q object down to the tuple.  Hopefully written in a way that can handle changes to model settings. Started working out how to test the exclude.  Still bouncing around ideas for how to build the filter.

--- web/apps/inventory/tests/test_source_item_wide_filter.py
+++ web/apps/inventory/tests/test_source_item_wide_filter.py
@@ -22,7 +22,61 @@ class SourceItemWideFilterTestCase(test.TransactionTestCase):
     def test_get_combined_filter(self):
         search_terms = [
             ('name', ('term1', 'term2')),
-            ('unit_size', ('8oz')),
+            ('unit_size', ('8oz', )),
         ]
-        obj = inv_models.SourceItem.objects.get_combined_filter(search_terms)
-        self.assertIsInstance(obj, models.Q)
+        combined_filter = inv_models.SourceItem.objects.get_combined_filter(search_terms)
+        self.validate_combined_filter(combined_filter, search_terms)
+
+    def validate_combined_filter(self, combined_filter, search_terms):
+        # <Q: (
+        #   AND: (
+        #       OR:
+        #         (AND: ('cryptic_name__icontains', 'term1'), ('cryptic_name__icontains', 'term2')),
+        #         (AND: ('verbose_name__icontains', 'term1'), ('verbose_name__icontains', 'term2')),
+        #         (AND: ('common_name__icontains', 'term1'), ('common_name__icontains', 'term2'))
+        #        ),
+        #       ('unit_size__icontains', '8oz'))>
+        self.assertIsInstance(combined_filter, models.Q)
+        self.assertEquals(len(combined_filter.children), len(search_terms))
+        self.assertEquals(combined_filter.connector, 'AND')
+
+        name_filter = combined_filter.children[0]
+        self.assertIsInstance(name_filter, models.Q)
+        self.assertEquals(len(name_filter.children), len(inv_models.SourceItem.wide_filter_fields['name']))
+        self.assertEquals(name_filter.connector, 'OR')
+
+        # Fancy way to validate the correct Q/tuple values are in the name filter.  Because lists are used, things
+        # should always be in a predictable order.
+        # multiple database fields and multiple terms makes this a bit nested.
+        for name_filter_i, db_field_name in enumerate(inv_models.SourceItem.wide_filter_fields['name']):
+            name_field_filter = name_filter.children[name_filter_i]
+            self.assertEquals(len(name_field_filter.children), len(search_terms[0][1]))  # ('term1', 'term2')
+            self.assertIsInstance(name_field_filter, models.Q)
+            self.assertEquals(name_field_filter.connector, 'AND')
+            for i, name_field_filter_tuple in enumerate(name_field_filter.children):
+                with self.subTest(db_field_name=db_field_name, name_field_filter_tuple=name_field_filter_tuple, i=i):
+                    self.assertIsInstance(name_field_filter_tuple, tuple)
+                    self.assertEquals(name_field_filter_tuple[0], f"{db_field_name}__icontains")
+                    self.assertEquals(name_field_filter_tuple[1], search_terms[0][1][i])
+
+        # unit_size is simple as there's only one term and one field tested.
+        unit_size_filter = combined_filter.children[1]
+        self.assertIsInstance(unit_size_filter, tuple)
+        self.assertEquals(unit_size_filter[0], f"{search_terms[1][0]}__icontains")
+        self.assertEquals(unit_size_filter[1], search_terms[1][1][0])
+
+    def test_get_combined_filter_with_exclude(self):
+        search_terms = [
+            ('name', ('term1', 'term2')),
+            ('unit_size', ('8oz', )),
+            ('-name', ('exclude_term1', 'exclude_term2')),
+        ]
+        only_inclusive_search_terms = [st for st in search_terms if not st[0].startswith("-")]
+        only_exclusive_search_terms = [st for st in search_terms if st[0].startswith("-")]
+        combined_filter = inv_models.SourceItem.objects.get_combined_filter(search_terms)
+        self.assertIsInstance(combined_filter, models.Q)
+        self.validate_combined_filter(combined_filter, only_inclusive_search_terms)
+
+        combined_exclude = inv_models.SourceItem.objects.get_combined_filter(search_terms, exclude_filter=True)
+        print(f"\ncombined_exclude == {combined_exclude}")
+        self.validate_combined_filter(combined_exclude, only_exclusive_search_terms)
