c7e5c9a7919d29045de7692f13d88d0a78ea12bc
Adam Torres <adamprogrammer@gmail.com>
2025-01-20 08:26:27 -0700

    Made debug respond to environment changes a bit.

--- web/config/settings.py
+++ web/config/settings.py
@@ -30,7 +30,7 @@ sys.path.append(str(BASE_DIR / 'apps'))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DJANGO_DEBUG') or "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -130,7 +130,7 @@ LOGGING = {
     'loggers': {
         '': {
             'handlers': ['console'],
-            'level': 'DEBUG',
+            'level': 'DEBUG' if DEBUG else 'INFO',
         },
     }
 }
